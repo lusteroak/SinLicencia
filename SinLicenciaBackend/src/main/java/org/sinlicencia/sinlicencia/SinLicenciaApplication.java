399b5bc77bcf0b1a8c2451144fc9b6a5082381c9
lusteroak <lusteroak@protonmail.com>
2026-06-05 13:27:14 -0500

    Initial commit

---
+++ SinLicenciaBackend/src/main/java/org/sinlicencia/sinlicencia/SinLicenciaApplication.java
@@ -0,0 +1,13 @@
+package org.sinlicencia.sinlicencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinLicenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinLicenciaApplication.class, args);
+    }
+
+}
---
+++ SinLicenciaBackend/src/test/java/org/sinlicencia/sinlicencia/SinLicenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sinlicencia.sinlicencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinLicenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
