eb4ec35be094ce91b796e7dc5dca139adb9eca04
lusteroak <lusteroak@protonmail.com>
2026-06-07 21:32:58 -0500

    Added font awesome, update header

--- SinLicenciaFrontEnd/src/app/components/header/header.ts
+++ SinLicenciaFrontEnd/src/app/components/header/header.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { SearchBarHeader } from '../search-bar-header/search-bar-header';
 import { LogoPlaceHolder } from '../logo-place-holder/logo-place-holder';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
-  imports: [SearchBarHeader, LogoPlaceHolder],
+  imports: [SearchBarHeader, LogoPlaceHolder, FontAwesomeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  faCartShopping = faCartShopping;
 }
